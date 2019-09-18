c387bc53a88a2f7360ae5f61a54c3c86d23dbaba
anubhavmishra1 <39110231+anubhavmishra1@users.noreply.github.com>
2019-09-18 18:55:07 +0530

    Random number feature

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss']
+})
+export class SquareComponent implements OnInit {
+rando =Math.random();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
